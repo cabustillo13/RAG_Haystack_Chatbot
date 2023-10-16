1dd40f1f22a40f186a46dd68790cdc614ac69781
cabustillo13 <cabustillo13@hotmail.com>
2023-10-16 14:32:51 -0300

    [UPDATE] Add Lyrics dataset

--- src/etl/extract.py
+++ src/etl/extract.py
@@ -16,9 +16,15 @@ def extract_data(kaggle_dataset_id):
         # Connect to Kaggle to download the database
         kaggle.api.authenticate()
         kaggle.api.dataset_download_files(kaggle_dataset_id, path='./', unzip=True)
+        
         # Convert it into a dataframe
-        df = pd.read_csv("netflix_titles.csv")
-        return df
+        df_bts = pd.read_csv("./csv/BTS.csv")
+        df_lady_gaga = pd.read_csv("./csv/LadyGaga.csv")
+
+        # Concatenate 2 dataframes
+        df_lyrics = pd.concat([df_bts, df_lady_gaga], axis=0)
+
+        return df_lyrics
     except:
         raise Exception(f"Error retrieving data from {kaggle_dataset_id}")
 
@@ -35,11 +41,11 @@ def save_to_duckdb(df, table_name, db_path):
 
 if __name__ == "__main__":
 
-    # Extract data from Kaggle ID dataset: https://www.kaggle.com/datasets/shivamb/netflix-shows
-    kaggle_dataset_id = "shivamb/netflix-shows"
+    # Extract data from Kaggle ID dataset: https://www.kaggle.com/datasets/deepshah16/song-lyrics-dataset
+    kaggle_dataset_id = "deepshah16/song-lyrics-dataset"
     df = extract_data(kaggle_dataset_id)
     
     # Save to duckdb
     db_path = "data.duckdb"
-    table_name = "netflix_shows"
+    table_name = "song_lyrics"
     save_to_duckdb(df, table_name, db_path)
